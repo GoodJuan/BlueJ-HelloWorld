e5d09a6bce6d50d0e6a8b9865a7673ea19947320
GoodJuan <190141@hkis.edu.hk>
2016-08-22 10:34:28 +0800

    init
    init project

---
+++ BaconClass.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class BaconClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BaconClass
+{
+    public static void main(String[] args){
+        System.out.println("Archit is a foogit");
+        
+        
+    }
+}
